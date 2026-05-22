55a165a2dc6669717af476ece60347d865554f2c
Favour Oboh <favouroboh77@gmail.com>
2026-05-22 14:27:11 +0100

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,13 @@
+================================================================
+  Create database and schemas 
+================================================================
+CREATE DATABASE RetailWarehouse;
+
+USE RetailWarehouse ;
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
