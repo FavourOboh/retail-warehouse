eff331fdc93edb420a8992ddf08eeb3dfad426f6
Favour Oboh <favouroboh77@gmail.com>
2026-06-02 14:30:21 +0100

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -4,3 +4,20 @@
  restricted due to OS security policy; data ingested via ETL tool.
 ======================================================================
 ======================================================================
+
+===========================================================================================================
+--Checking for nulls and duplicates , i found that there were in fact 300 duplicates of my primary key.
+I proceeded to do this
+============================================================================================================
+SELECT DISTINCT
+*
+INTO orders_cleaned
+FROM bronze.mrw
+
+SELECT 
+COUNT(*)
+FROM bronze_mrw;
+SELECT 
+COUNT(*)
+FROM orders_cleaned;
+
