ac27c98fbdbde6a246333462f3a89f37b1d4ac25
Favour Oboh <favouroboh77@gmail.com>
2026-06-09 12:49:33 +0100

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -37,3 +37,72 @@ WHERE LOWER(TRIM(country)) = 'kenya';
 UPDATE dbo.orders_cleaned
 SET country = 'Ghana'
 WHERE LOWER(TRIM(country)) = 'ghana';
+
+============================================================================================================
+  fix lettering for customer segment
+============================================================================================================
+UPDATE dbo.orders_cleaned
+SET customer_segment = 'Corporate'
+WHERE LOWER(TRIM(customer_segment)) = 'corporate'
+
+UPDATE dbo.orders_cleaned
+SET customer_segment = 'Consumer'
+WHERE LOWER(TRIM(customer_segment)) = 'consumer'
+
+===========================================================================================================
+  product_category
+===========================================================================================================
+UPDATE dbo.orders_cleaned
+SET product_category = 'Beauty'
+WHERE LOWER(TRIM(product_category)) = 'beauty'
+
+UPDATE dbo.orders_cleaned
+SET product_category = 'Electronics'
+WHERE LOWER(TRIM(product_category)) = 'electronics'
+
+UPDATE dbo.orders_cleaned
+SET product_category = 'Fashion'
+WHERE LOWER(TRIM(product_category)) = 'fashion'
+
+UPDATE dbo.orders_cleaned
+SET product_category = 'Home'
+WHERE LOWER(TRIM(product_category)) = 'home'
+
+=========================================================================================================
+  payment method
+=========================================================================================================
+UPDATE dbo.orders_cleaned
+SET payment_method = 'Card'
+WHERE LOWER(TRIM(payment_method)) = 'card'
+
+UPDATE dbo.orders_cleaned
+SET payment_method = 'Cash'
+WHERE LOWER(TRIM(payment_method)) = 'cash'
+
+UPDATE dbo.orders_cleaned
+SET payment_method = 'Transfer'
+WHERE LOWER(TRIM(payment_method)) = 'transfer'
+
+==========================================================================================================
+  order_status
+==========================================================================================================
+UPDATE dbo.orders_cleaned 
+SET order_status = 'Cancelled'
+WHERE LOWER(TRIM(order_status)) = 'cancelled'
+
+UPDATE dbo.orders_cleaned 
+SET order_status = 'Completed'
+WHERE LOWER(TRIM(order_status)) = 'completed'
+
+UPDATE dbo.orders_cleaned 
+SET order_status = 'Returned'
+WHERE LOWER(TRIM(order_status)) = 'returned'
+
+==========================================================================================================
+Fixing negatives in quantity
+==========================================================================================================
+
+UPDATE dbo.orders_cleaned
+SET quantity = ABS(quantity)
+WHERE quantity < 0;
+
